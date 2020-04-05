5a674a0777d4609ef485316b87a9fb0581259e48
kfo42 <kfo@stanford.edu>
2020-04-04 17:04:29 -0700

    Update FacebookFrame.js

--- src/components/FacebookFrame.js
+++ src/components/FacebookFrame.js
@@ -1,16 +1,17 @@
 import React from 'react';
 
-const Iframe = ({ source }) => {
+const Iframe = ({ title, source }) => {
 
     if (!source) {
         return <div>Loading...</div>;
     }
-
+    
+    const siteTitle = title
     const src = source;     
     return (
         <div className="col-md-12">
             <div className="emdeb-responsive">
-                <iframe src={src}></iframe>
+                <iframe title = {siteTitle} src={src}></iframe>
             </div>
         </div>
     );
