22ab9c928748dc714d201ef7dde8af2f6b2ceaa0
Katie Fo <kfo@stanford.edu>
2020-04-07 16:13:51 -0700

    Update Chat.js

--- src/pages/Chat.js
+++ src/pages/Chat.js
@@ -36,16 +36,19 @@ const Chat = () => {
     
      <TextSection title='Find Co-caring Projects'>
     
+        <div class="embedly-card"><h4><a href="https://www.facebook.com/groups/cocaring/learning_content/?filter=2668631300033869">Getting Started: Advice & How To</a></h4><p>Useful models and dashboards for getting started with co-caring. Stories on what other people have done to get their #co-caring for COVID-19 projects launched from idea to reality.</p></div>
+          <blockquote class="embedly-card"><h4><a href="https://www.facebook.com/groups/cocaring/permalink/221760115808473/"></a></h4><p>Rituals for the Pandemic: A model for getting started</p></blockquote>
+
         <div class="embedly-card"><h4><a href="https://www.facebook.com/groups/cocaring/learning_content/?filter=531981410841637">Requests for Support: Needs</a></h4><p>Learn about the different needs people have, and consider starting a co-caring support project.</p></div>
 
         <div class="embedly-card"><h4><a href="https://www.facebook.com/groups/cocaring/learning_content/?filter=532557444068356">Active U.S. Co-Caring Projects</a></h4><p>Learn about co-caring COVID-19 efforts active nationwide in the U.S.</p></div>
 
-        <blockquote class="embedly-card"><h4><a href="https://www.facebook.com/groups/cocaring/permalink/222503875734097/">Mutual Aid Disaster Relief: a list of mutual aid resources by country and state or province></a></h4></blockquote>
-        <blockquote class="embedly-card"><h4><a href="https://www.facebook.com/groups/cocaring/permalink/221200022531149/">Salesforce and 1BPPE.com partnership to provide PPE equipment</a></h4></blockquote>
-        <blockquote class="embedly-card"><h4><a href="https://www.facebook.com/groups/cocaring/permalink/222503435734141/">Reach4Help: A map of mutual aid resources with links.</a></h4></blockquote>
-        <blockquote class="embedly-card"><h4><a href="https://www.facebook.com/groups/cocaring/permalink/222773585707126/">Giving and receiving support for immunocomprised people.</a></h4></blockquote>
-        <blockquote class="embedly-card"><h4><a href="https://www.facebook.com/groups/cocaring/permalink/219853725999112/">Covid Assist: neighbors helping neighbors through the coronavirus pandemic.</a></h4></blockquote>
-        <blockquote class="embedly-card"><h4><a href="https://www.facebook.com/groups/cocaring/permalink/219426259375192/">Sponsor an N95 Mask for a COVID Nurse/Doctor in an NYC Hospital</a></h4></blockquote>
+          <blockquote class="embedly-card"><h4><a href="https://www.facebook.com/groups/cocaring/permalink/222503875734097/">Mutual Aid Disaster Relief: a list of mutual aid resources by country and state or province></a></h4></blockquote>
+          <blockquote class="embedly-card"><h4><a href="https://www.facebook.com/groups/cocaring/permalink/221200022531149/">Salesforce and 1BPPE.com partnership to provide PPE equipment</a></h4></blockquote>
+          <blockquote class="embedly-card"><h4><a href="https://www.facebook.com/groups/cocaring/permalink/222503435734141/">Reach4Help: A map of mutual aid resources with links.</a></h4></blockquote>
+          <blockquote class="embedly-card"><h4><a href="https://www.facebook.com/groups/cocaring/permalink/222773585707126/">Giving and receiving support for immunocomprised people.</a></h4></blockquote>
+          <blockquote class="embedly-card"><h4><a href="https://www.facebook.com/groups/cocaring/permalink/219853725999112/">Covid Assist: neighbors helping neighbors through the coronavirus pandemic.</a></h4></blockquote>
+          <blockquote class="embedly-card"><h4><a href="https://www.facebook.com/groups/cocaring/permalink/219426259375192/">Sponsor an N95 Mask for a COVID Nurse/Doctor in an NYC Hospital</a></h4></blockquote>
 
 
 
