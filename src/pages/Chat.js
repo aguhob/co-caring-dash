9f3f202a7e59823d25e04045fa3261ed031d2cd3
Katie Fo <kfo@stanford.edu>
2020-04-07 15:26:23 -0700

    Update Chat.js

--- src/pages/Chat.js
+++ src/pages/Chat.js
@@ -56,7 +56,7 @@ const Chat = () => {
         <div class="embedly-card"><h4><a href="https://www.facebook.com/groups/cocaring/learning_content/?filter=841147669726184">Active California Co-Caring Projects</a></h4><p>Learn about co-caring COVID-19 efforts active in California.</p></div>    
 
         
-        <div class="embedly-card"><h4><a href="https://www.facebook.com/groups/cocaring/learning_content/?filter=219990582437381">General Covid-19 Info</a></h4><p>We are curating the best, latest info about coronavirus and COVID-19. Learn here.</p></blockquote>
+        <div class="embedly-card"><h4><a href="https://www.facebook.com/groups/cocaring/learning_content/?filter=219990582437381">General Covid-19 Info</a></h4><p>We are curating the best, latest info about coronavirus and COVID-19. Learn here.</p></div>
         <div class="embedly-card"><h4><a href="https://www.facebook.com/groups/cocaring/learning_content/?filter=263683064646675">Mutual Aid Networks</a></h4><p>Learn about other organizations, communities, maps and tools who are connecting people to help.</p></div>
         <div class="embedly-card"><h4><a href="https://www.facebook.com/groups/cocaring/learning_content/?filter=551804562134697">Relief Funds</a></h4><p>Learn about additional funding available for vulnerable populations and organizations.</p></div>
 
