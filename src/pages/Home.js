c7f8d41256c1f23fcec07512f024484b86ec3af4
Katie Fo <kfo@stanford.edu>
2020-04-09 19:11:55 -0700

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -29,10 +29,7 @@ const Home = () => {
         <p>As private individuals and as organizations, our current situation calls us to both. In response to the ongoing challenge to track the spread of COVID-19 and connect individuals, organizations and communities who want to help, we have created the #Co-Caring Dashboard.</p>
         <p>To use the #Co-Caring Dashboard, please scroll down for information about COVID-19 in your state and county.</p>
       </TextSection>
-    
-      <TextSection title="TRACKING CORONAVIRUS"/>
-
-           
+              
 
 
       <TrendingList>
